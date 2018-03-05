88e11b58bfffed8f34e23d1c50e6ef22dde67425
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-03-05 19:10:17 +0700

    clear console.log

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
-// const jwt = require('jsonwebtoken');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
